887487a128b50f2ef9bb78188370ff4ed777ef4a
Hellen Moura <127620071+hellen-moura@users.noreply.github.com>
2024-04-08 20:19:07 -0300

    Add files via upload

---
+++ PYTHON/MUNDO_01/ex002.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input("Digite seu nome: ")
+print('É um prazer em te conhecer, {}!'.format(nome))
+#.format -> o nome é formatado para caber nas chaves vazias
