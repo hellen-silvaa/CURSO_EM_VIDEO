ec6f3f11d4c0dd1cfd95da3d4c82a208bc2c789f
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-05-02 22:53:38 -0300

    Delete PYTHON/MUNDO_01/ex0014.py

--- PYTHON/MUNDO_01/ex0014.py
+++
@@ -1,4 +0,0 @@
-#Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
-c = float(input('Informe a temperatura em ºC: '))
-f = 9 * c / 5 + 32
-print(f'A temperatura de {c}ºC corresponde a {f}ºF')
