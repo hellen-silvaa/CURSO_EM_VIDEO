b677a2b94400de9e46c1d4cc32e3b86ffb5129ab
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-20 20:30:12 -0300

    finalizando desafio 22

--- PYTHON/MUNDO_01/ex022.py
+++ PYTHON/MUNDO_01/ex022.py
@@ -3,3 +3,13 @@
 #- O nome com todas as letras minúsculas
 #- Quantas letras ao todo (sem considerar espaços)
 #- Quantas letras tem o primeiro nome
+
+nome_completo = str(input('Digite seu nome completo: '))
+print(f'Seu nome em maiúsculas é {nome_completo.upper()}' 
+        f'Seu nome em minúsculas é {nome_completo.lower()}'
+        f'Seu nome tem ao todo {len(nome_completo) - nome_completo.count(" ")} letras'
+        f'Seu primeiro nome tem {nome_completo.find(" ")} letras'
+     )
+
+separa = nome_completo.split()
+print(f'Seu primeiro nome é {separa[0]} e ele tem {len(separa[0])} letras')
