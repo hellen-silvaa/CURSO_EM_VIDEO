e2f7b186ae476ea0f7d6dc92628abd142dd10c48
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-20 20:34:09 -0300

    finalizando exercicio 23

--- PYTHON/MUNDO_01/ex023.py
+++ PYTHON/MUNDO_01/ex023.py
@@ -4,3 +4,15 @@
 #Dezena: 3
 #Centena: 8
 #Milhar: 1
+
+num = int(input('Digite um número de 0 a 9999: '))
+#pega o número e divide por 1, 10, 100 e 1000, depois pega o resto da divisão por 10
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Analisando o número {num}')
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
