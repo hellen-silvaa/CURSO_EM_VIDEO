df5adf3df187c26faffc3a2e83fc27775473a537
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-20 20:38:47 -0300

    finalizando exercicio 26, facil

--- PYTHON/MUNDO_01/ex025.py
+++ PYTHON/MUNDO_01/ex025.py
@@ -1,1 +1,7 @@
 #Desafio 025: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+nome = str(input('Qual é o seu nome completo? ')).strip()
+
+print('SILVA' in nome.upper())
+
+# in é um operador que verifica se um valor está contido em outro valor
