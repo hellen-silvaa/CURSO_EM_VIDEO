3879097b9c9015c1dec4c873e16cb3a064c7a770
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-20 20:40:56 -0300

    finalizando exercicio 26

--- PYTHON/MUNDO_01/ex026.py
+++ PYTHON/MUNDO_01/ex026.py
@@ -2,3 +2,8 @@
 #- Quantas vezes aparece a letra "A"
 #- Em que posição ela aparece a primeira vez
 #- Em que posição ela aparece a última vez
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+print(f'A letra "A" aparece {frase.count("A")} vezes na frase')
+print(f'A primeira letra "A" apareceu na posição {frase.find("A") + 1}')
+print(f'A última letra "A" apareceu na posição {frase.rfind("A") + 1}')
