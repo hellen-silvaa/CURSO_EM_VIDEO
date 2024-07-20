5b1ae8246caa7bdde12b0df7c85b95d63611de88
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-20 20:06:09 -0300

    manipulação de cadeias de texto

---
+++ PYTHON/MUNDO_01/manipulandoTexto.py
@@ -0,0 +1,56 @@
+
+# frase = 'Curso em Vídeo Python'
+
+#MANIPULAÇÃO
+
+#len() mostra o tamanho da string
+#exemplo: print(f'A frase tem {len(frase)} caracteres.')
+
+#count() conta quantas vezes a substring aparece na string
+#exemplo: print(f'A letra "a" aparece {frase.count("a")} vezes na frase.')
+
+#find() mostra a posição da substring na string
+#exemplo: print(f'A primeira letra "a" aparece na posição {frase.find("a")}.')
+
+#-------------------------------------------------------------------------------
+
+#TRANSFORMAÇÃO
+
+#replace() substitui a substring por outra
+#exemplo: print(f'A frase ficou assim: {frase.replace("Python", "Android")}.')
+
+#upper() transforma a string em maiúsculas
+#exemplo: print(f'A frase em maiúsculas: {frase.upper()}.')
+
+#lower() transforma a string em minúsculas
+#exemplo: print(f'A frase em minúsculas: {frase.lower()}.')
+
+#capitalize() transforma a primeira letra da string em maiúscula
+#exemplo: print(f'A frase com a primeira letra em maiúscula: {frase.capitalize()}.')
+
+#title() transforma a primeira letra de cada palavra em maiúscula
+#exemplo: print(f'A frase com a primeira letra de cada palavra em maiúscula: {frase.title()}.')
+
+#strip() remove espaços no início e no final da string
+#exemplo: print(f'A frase sem espaços no início e no final: {frase.strip()}.')
+
+#rstrip() remove espaços à direita da string
+#exemplo: print(f'A frase sem espaços à direita: {frase.rstrip()}.')
+
+#lstrip() remove espaços à esquerda da string
+#exemplo: print(f'A frase sem espaços à esquerda: {frase.lstrip()}.')
+
+#-------------------------------------------------------------------------------
+
+#DIVISÃO
+
+#split() divide a string em uma lista de strings
+#exemplo: print(f'A frase dividida em palavras: {frase.split()}.')
+
+#-------------------------------------------------------------------------------
+
+#JUNÇÃO
+
+#join() junta uma lista de strings em uma única string
+#exemplo: print(''.join(frase))
+
