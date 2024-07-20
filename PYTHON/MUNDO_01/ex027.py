133e928ce0b21b01fde69942096d5a3e56ac5189
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-20 20:45:26 -0300

    finalizando exercicio 27

--- PYTHON/MUNDO_01/ex027.py
+++ PYTHON/MUNDO_01/ex027.py
@@ -2,3 +2,7 @@
 #Ex: Ana Maria de Souza
 #primeiro = Ana
 #último = Souza
+
+nome = str(input('Digite o seu nome completo: ')).strip().split()
+print(f'Primeiro nome: {nome[0]}')
+print(f'Último nome: {nome[len(nome) - 1]}')
