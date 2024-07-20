83870aacdf9e3ea399b3e4e4b36af0a64240a4d4
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-20 18:28:07 -0300

    teste

--- PYTHON/MUNDO_01/ex021.py
+++ PYTHON/MUNDO_01/ex021.py
@@ -1,11 +1,5 @@
 #Desafio 021: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+#playsound é uma função que reproduz um arquivo de áudio
 
-#pygame é uma biblioteca de funções para jogos
-import pygame
-#init inicializa o pygame
-pygame.init()
-#mixer é uma função para música
-pygame.mixer.music.load('ex021.mp3')
-#play inicia a música
-pygame.mixer.music.play()
-pygame
+from playsound import playsound
+playsound ('ex021.mp3') 
