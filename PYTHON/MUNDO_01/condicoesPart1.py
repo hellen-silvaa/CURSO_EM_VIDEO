d49e530470932717e47a7dfd122becbc7fab7d85
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-22 14:12:09 -0300

    sequencial e condicional

--- PYTHON/MUNDO_01/condicoesPart1.py
+++ PYTHON/MUNDO_01/condicoesPart1.py
@@ -1,1 +1,3 @@
-.
+#Estruturas sequenciais e condicionais
+# Estrutura sequencial é uma sequência de comandos que são executados um após o outro.
+# Estrutura condicional é uma sequência de comandos que são executados de acordo com uma condição.
