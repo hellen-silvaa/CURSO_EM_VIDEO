3d551478c5beefce20c00e08bab0c44cb237407c
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-22 15:08:37 -0300

    finalizando exercicio 028

--- PYTHON/MUNDO_01/ex028.py
+++ PYTHON/MUNDO_01/ex028.py
@@ -1,12 +1,16 @@
 #Desafio 028: Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
+#sleep = faz o programa esperar um tempo em segundos
 #randint = gera números inteiros aleatórios
 from random import randint
+from time import sleep
 computador = randint(0, 5)
 print('-=-' * 20)
 print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
 print('-=-' * 20)
 jogador = int(input('Em que número eu pensei? '))
+print('Processando...')
+sleep(3)
 if jogador == computador:
     print('Parabéns! Você conseguiu me vencer!')
 else:
