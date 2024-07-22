f414ee82f97bb42bb70c58d81af532a206af496f
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-22 18:24:39 -0300

    exercicio 34 finalizado

--- PYTHON/MUNDO_01/ex034.py
+++ PYTHON/MUNDO_01/ex034.py
@@ -1,1 +1,8 @@
 #Desafio 034: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1.250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+
+salario = float(input('Digite o salário do funcionário: R$'))
+if salario > 1250:
+    aumento = salario + (salario * 10 / 100)
+else:
+    aumento = salario + (salario * 15 / 100)
+print(f'O salário de R${salario:.2f} com o aumento passa a ser R${aumento:.2f}.')
