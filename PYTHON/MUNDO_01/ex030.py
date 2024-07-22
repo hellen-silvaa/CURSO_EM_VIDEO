b047f19b1c718acc2f2643b90bf773a80b0835d8
Hellen Moura <hellen.aparecidams@outlook.com>
2024-07-22 15:24:28 -0300

    exercicio 030 finalizado

--- PYTHON/MUNDO_01/ex030.py
+++ PYTHON/MUNDO_01/ex030.py
@@ -1,1 +1,8 @@
 #Desafio 030: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+num = int(input('Me diga um número qualquer: '))
+if num % 2 == 0:
+    print(f'O número {num} é PAR!')
+else:
+    print(f'O número {num} é ÍMPAR!')
+    
